35789bc261ae2585cc726f24359e8f15f1c56209
Joe Hirsch <hirsch.jos@northeastern.edu>
2023-11-28 14:25:11 -0500

    test stubs

---
+++ Q/src/json/data/dataTypeGuard.test.ts
@@ -0,0 +1,35 @@
+describe('tests for the json data defintions type guards', () => {
+  test('isJState', () => {
+    expect(true).toBe(true);
+  });
+  test('isJPub', () => {
+    expect(true).toBe(true);
+  });
+  test('isJMap', () => {
+    expect(true).toBe(true);
+  });
+  test('isJChoice', () => {
+    expect(true).toBe(true);
+  });
+  test('isJPlacements', () => {
+    expect(true).toBe(true);
+  });
+  test('isJTile', () => {
+    expect(true).toBe(true);
+  });
+  test('isJActorsB', () => {
+    expect(true).toBe(true);
+  });
+  test('isSimpleJActor', () => {
+    expect(true).toBe(true);
+  });
+  test('isExceptionJActor', () => {
+    expect(true).toBe(true);
+  });
+  test('isCheatJActor', () => {
+    expect(true).toBe(true);
+  });
+  test('isLoopJActor', () => {
+    expect(true).toBe(true);
+  });
+});
