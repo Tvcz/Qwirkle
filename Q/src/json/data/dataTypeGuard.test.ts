98e5d204f44323573882b9047911f7a50f64c1a2
Nicolas Araujo <araujo.n@northeastern.edu>
2023-11-28 19:21:41 -0500

    add tests for JActorsSpecB and and JActorsB

--- Q/src/json/data/dataTypeGuard.test.ts
+++ Q/src/json/data/dataTypeGuard.test.ts
@@ -1,4 +1,14 @@
-import { isJChoice, isJMap, isJPlacements, isJTile } from './dataTypeGuards';
+import {
+  isCheatJActor,
+  isExceptionJActor,
+  isJActorsB,
+  isJChoice,
+  isJMap,
+  isJPlacements,
+  isJTile,
+  isLoopJActor,
+  isSimpleJActor
+} from './dataTypeGuards';
 
 describe('tests for the json data defintions type guards', () => {
   test('isJState', () => {
@@ -219,22 +229,354 @@ describe('tests for the json data defintions type guards', () => {
   });
 
   test('isJActorsB', () => {
-    expect(true).toBe(true);
+    // Valid JActorsB object - 2 actors
+    expect(
+      isJActorsB([
+        ['simpleJActor1', 'dag'],
+        ['simpleJActor2', 'ldasg']
+      ])
+    ).toBe(true);
+
+    // Valid JActorsB object - 3 actors
+    expect(
+      isJActorsB([
+        ['simpleJActor1', 'dag'],
+        ['simpleJActor2', 'ldasg'],
+        ['simpleJActor3', 'dag']
+      ])
+    ).toBe(true);
+
+    // Valid JActorsB object - 4 actors
+    expect([
+      ['simpleJActor', 'dag'],
+      ['exceptionJactor', 'ldasg', 'new-tiles'],
+      ['cheatJactor', 'dag', 'a cheat', 'tile-not-owned'],
+      ['loopJactor', 'ldasg', 'win', 2]
+    ]).toBe(true);
+
+    // Invalid JActorsB object - 1 actor
+    expect(isJActorsB([['simpleJActor', 'dag']])).toBe(false);
+
+    // Invalid JActorsB object - 5 actors
+    expect(
+      isJActorsB([
+        ['simpleJActor1', 'dag'],
+        ['simpleJActor2', 'ldasg'],
+        ['simpleJActor3', 'dag'],
+        ['simpleJActor4', 'ldasg'],
+        ['simpleJActor5', 'dag']
+      ])
+    ).toBe(false);
+
+    // Invalid JActorsB object - null value
+    expect(isJActorsB(null)).toBe(false);
+
+    // Invalid JActorsB object - non-array value
+    expect(isJActorsB('red')).toBe(false);
+
+    // Invalid JActorsB object - wrong JExn value
+    expect(
+      isJActorsB([
+        ['simpleJActor', 'dag'],
+        ['exceptionJactor', 'ldasg', 'take turn'], // missing "-"
+        ['cheatJactor', 'dag', 'a cheat', 'tile-not-owned'],
+        ['loopJactor', 'ldasg', 'win', 2]
+      ])
+    ).toBe(false);
+
+    // Invalid JActorsB object - wrong JCheat value
+    expect(
+      isJActorsB([
+        ['simpleJActor', 'dag'],
+        ['exceptionJactor', 'ldasg', 'new-tiles'],
+        ['cheatJactor', 'dag', 'a cheat', 'no fit'], // missing "-"
+        ['loopJactor', 'ldasg', 'win', 2]
+      ])
+    ).toBe(false);
+
+    // Invalid JActorsB object - wrong JStrategy value
+    expect(
+      isJActorsB([
+        ['simpleJActor', 'dag'],
+        ['exceptionJactor', 'invalid', 'new-tiles'],
+        ['cheatJactor', 'dag', 'a cheat', 'tile-not-owned'],
+        ['loopJactor', 'ldasg', 'win', 2]
+      ])
+    ).toBe(false);
+
+    // Invalid JActorsB object - JName too long
+    expect(
+      isJActorsB([
+        ['simpleJActor', 'dag'],
+        ['exceptionJactor', 'ldasg', 'new-tiles'],
+        ['nameTooLongJactoraaaa', 'dag', 'a cheat', 'tile-not-owned'],
+        ['loopJactor', 'ldasg', 'win', 2]
+      ])
+    ).toBe(false);
+
+    // Invalid JActorsB object - JName too short
+    expect(
+      isJActorsB([
+        ['simpleJActor', 'dag'],
+        ['exceptionJactor', 'ldasg', 'new-tiles'],
+        ['', 'dag', 'a cheat', 'tile-not-owned'],
+        ['loopJactor', 'ldasg', 'win', 2]
+      ])
+    ).toBe(false);
+
+    // Invalid JActorsB object - JName not a string
+    expect(
+      isJActorsB([
+        ['simpleJActor', 'dag'],
+        ['exceptionJactor', 'ldasg', 'new-tiles'],
+        [1, 'dag', 'a cheat', 'tile-not-owned'],
+        ['loopJactor', 'ldasg', 'win', 2]
+      ])
+    ).toBe(false);
+
+    // Invalid JActorsB object - JName contains invalid character
+    expect(
+      isJActorsB([
+        ['simpleJActor', 'dag'],
+        ['exceptionJactor', 'ldasg', 'new-tiles'],
+        ['name with space', 'dag', 'a cheat', 'tile-not-owned'],
+        ['loopJactor', 'ldasg', 'win', 2]
+      ])
+    ).toBe(false);
   });
 
   test('isSimpleJActor', () => {
-    expect(true).toBe(true);
+    // Valid SimpleJActor object
+    expect(isSimpleJActor(['simpleJActor', 'dag'])).toBe(true);
+
+    // Valid SimpleJActor object
+    expect(isSimpleJActor(['simpleJActor', 'ldasg'])).toBe(true);
+
+    // Invalid SimpleJActor object - null value
+    expect(isSimpleJActor(null)).toBe(false);
+
+    // Invalid SimpleJActor object - non-array value
+    expect(isSimpleJActor('red')).toBe(false);
+
+    // Invalid SimpleJActor object - JName too long
+    expect(isSimpleJActor(['nameTooLongJactoraaaa', 'dag'])).toBe(false);
+
+    // Invalid SimpleJActor object - JName too short
+    expect(isSimpleJActor(['', 'dag'])).toBe(false);
+
+    // Invalid SimpleJActor object - JName not a string
+    expect(isSimpleJActor([1, 'dag'])).toBe(false);
+
+    // Invalid SimpleJActor object - JName contains invalid character
+    expect(isSimpleJActor(['name with space', 'dag'])).toBe(false);
+
+    // Invalid SimpleJActor object - JName contains invalid character
+    expect(isSimpleJActor(['name-with-hyphen', 'dag'])).toBe(false);
+
+    // Invalid SimpleJActor object - JName contains invalid character
+    expect(isSimpleJActor(['name_with_underscore', 'dag'])).toBe(false);
+
+    // Invalid SimpleJActor object - wrong JStrategy value
+    expect(isSimpleJActor(['simpleJActor', 'invalid'])).toBe(false);
+
+    // Invalid SimpleJActor object - JStrategy not a string
+    expect(isSimpleJActor(['simpleJActor', 1])).toBe(false);
   });
 
   test('isExceptionJActor', () => {
-    expect(true).toBe(true);
+    // Valid ExceptionJActor object
+    expect(isExceptionJActor(['exceptionJactor', 'dag', 'new-tiles'])).toBe(
+      true
+    );
+
+    // Valid ExceptionJActor object
+    expect(isExceptionJActor(['exceptionJactor', 'ldasg', 'take-turn'])).toBe(
+      true
+    );
+
+    // Invalid ExceptionJActor object - null value
+    expect(isExceptionJActor(null)).toBe(false);
+
+    // Invalid ExceptionJActor object - non-array value
+    expect(isExceptionJActor('red')).toBe(false);
+
+    // Invalid ExceptionJActor object - JName too long
+    expect(
+      isExceptionJActor(['nameTooLongJactoraaaa', 'dag', 'new-tiles'])
+    ).toBe(false);
+
+    // Invalid ExceptionJActor object - JName too short
+    expect(isExceptionJActor(['', 'dag', 'new-tiles'])).toBe(false);
+
+    // Invalid ExceptionJActor object - JName not a string
+    expect(isExceptionJActor([1, 'dag', 'new-tiles'])).toBe(false);
+
+    // Invalid ExceptionJActor object - JName contains invalid character
+    expect(isExceptionJActor(['name with space', 'dag', 'new-tiles'])).toBe(
+      false
+    );
+
+    // Invalid ExceptionJActor object - JName contains invalid character
+    expect(isExceptionJActor(['name-with-hyphen', 'dag', 'new-tiles'])).toBe(
+      false
+    );
+
+    // Invalid ExceptionJActor object - JName contains invalid character
+    expect(
+      isExceptionJActor(['name_with_underscore', 'dag', 'new-tiles'])
+    ).toBe(false);
+
+    // Invalid ExceptionJActor object - wrong JStrategy value
+    expect(isExceptionJActor(['exceptionJactor', 'invalid', 'new-tiles'])).toBe(
+      false
+    );
+
+    // Invalid ExceptionJActor object - JStrategy not a string
+    expect(isExceptionJActor(['exceptionJactor', 1, 'new-tiles'])).toBe(false);
+
+    // Invalid ExceptionJActor object - wrong JExn value
+    expect(
+      isExceptionJActor(['exceptionJactor', 'ldasg', 'take turn']) // missing "-"
+    ).toBe(false);
+
+    // Invalid ExceptionJActor object - JExn not a string
+    expect(isExceptionJActor(['exceptionJactor', 'ldasg', 1])).toBe(false);
   });
 
   test('isCheatJActor', () => {
-    expect(true).toBe(true);
+    // Valid CheatJActor object
+    expect(
+      isCheatJActor(['cheatJactor', 'dag', 'a cheat', 'tile-not-owned'])
+    ).toBe(true);
+
+    // Valid CheatJActor object
+    expect(
+      isCheatJActor([
+        'cheatJactor',
+        'ldasg',
+        'a cheat',
+        'non-adjacent-coordinate'
+      ])
+    ).toBe(true);
+
+    // Invalid CheatJActor object - null value
+    expect(isCheatJActor(null)).toBe(false);
+
+    // Invalid CheatJActor object - non-array value
+    expect(isCheatJActor('red')).toBe(false);
+
+    // Invalid CheatJActor object - JName too long
+    expect(
+      isCheatJActor([
+        'nameTooLongJactoraaaa',
+        'dag',
+        'a cheat',
+        'tile-not-owned'
+      ])
+    ).toBe(false);
+
+    // Invalid CheatJActor object - JName too short
+    expect(isCheatJActor(['', 'dag', 'a cheat', 'tile-not-owned'])).toBe(false);
+
+    // Invalid CheatJActor object - JName not a string
+    expect(isCheatJActor([1, 'dag', 'a cheat', 'tile-not-owned'])).toBe(false);
+
+    // Invalid CheatJActor object - JName contains invalid character
+    expect(
+      isCheatJActor(['name with space', 'dag', 'a cheat', 'tile-not-owned'])
+    ).toBe(false);
+
+    // Invalid CheatJActor object - JName contains invalid character
+    expect(
+      isCheatJActor(['name-with-hyphen', 'dag', 'a cheat', 'tile-not-owned'])
+    ).toBe(false);
+
+    // Invalid CheatJActor object - JName contains invalid character
+    expect(
+      isCheatJActor([
+        'name_with_underscore',
+        'dag',
+        'a cheat',
+        'tile-not-owned'
+      ])
+    ).toBe(false);
+
+    // Invalid CheatJActor object - wrong JStrategy value
+    expect(
+      isCheatJActor(['cheatJactor', 'invalid', 'a cheat', 'tile-not-owned'])
+    ).toBe(false);
+
+    // Invalid CheatJActor object - JStrategy not a string
+    expect(isCheatJActor(['cheatJactor', 1, 'a cheat', 'tile-not-owned'])).toBe(
+      false
+    );
+
+    // Invalid CheatJActor object - "a cheat" not a string
+    expect(isCheatJActor(['cheatJactor', 'dag', 1, 'tile-not-owned'])).toBe(
+      false
+    );
+
+    // Invalid CheatJActor object - wrong JCheat value
+    expect(
+      isCheatJActor(['cheatJactor', 'dag', 'a cheat', 'no fit']) // missing "-"
+    ).toBe(false);
+
+    // Invalid CheatJActor object - JCheat not a string
+    expect(isCheatJActor(['cheatJactor', 'dag', 'a cheat', 1])).toBe(false);
   });
 
   test('isLoopJActor', () => {
-    expect(true).toBe(true);
+    // Valid LoopJActor object
+    expect(isLoopJActor(['loopJactor', 'dag', 'win', 2])).toBe(true);
+
+    // Valid LoopJActor object
+    expect(isLoopJActor(['loopJactor', 'ldasg', 'setup', 3])).toBe(true);
+
+    // Invalid LoopJActor object - null value
+    expect(isLoopJActor(null)).toBe(false);
+
+    // Invalid LoopJActor object - non-array value
+    expect(isLoopJActor('red')).toBe(false);
+
+    // Invalid LoopJActor object - JName too long
+    expect(isLoopJActor(['nameTooLongJactoraaaa', 'dag', 'win', 2])).toBe(
+      false
+    );
+
+    // Invalid LoopJActor object - JName too short
+    expect(isLoopJActor(['', 'dag', 'win', 2])).toBe(false);
+
+    // Invalid LoopJActor object - JName not a string
+    expect(isLoopJActor([1, 'dag', 'win', 2])).toBe(false);
+
+    // Invalid LoopJActor object - JName contains invalid character
+    expect(isLoopJActor(['name with space', 'dag', 'win', 2])).toBe(false);
+
+    // Invalid LoopJActor object - JName contains invalid character
+    expect(isLoopJActor(['name-with-hyphen', 'dag', 'win', 2])).toBe(false);
+
+    // Invalid LoopJActor object - JName contains invalid character
+    expect(isLoopJActor(['name_with_underscore', 'dag', 'win', 2])).toBe(false);
+
+    // Invalid LoopJActor object - wrong JStrategy value
+    expect(isLoopJActor(['loopJactor', 'invalid', 'win', 2])).toBe(false);
+
+    // Invalid LoopJActor object - JStrategy not a string
+    expect(isLoopJActor(['loopJactor', 1, 'win', 2])).toBe(false);
+
+    // Invalid LoopJActor object - wrong JExn value
+    expect(isLoopJActor(['loopJactor', 'ldasg', 'setup', 3])).toBe(false);
+
+    // Invalid LoopJActor object - JExn not a string
+    expect(isLoopJActor(['loopJactor', 'ldasg', 1, 3])).toBe(false);
+
+    // Invalid LoopJActor object - Count value too small
+    expect(isLoopJActor(['loopJactor', 'dag', 'win', 0])).toBe(false);
+
+    // Invalid LoopJActor object - Count value too large
+    expect(isLoopJActor(['loopJactor', 'dag', 'win', 8])).toBe(false);
+
+    // Invalid LoopJActor object - Count not a number
+    expect(isLoopJActor(['loopJactor', 'dag', 'win', '2'])).toBe(false);
   });
 });
