3d9e7be3ab10b0307e7ad81b022b23a4f2921899
Nicolas Araujo <araujo.n@northeastern.edu>
2023-11-30 14:51:00 -0500

    add JSDoc for toJPub and toJPubPlayers

--- Q/src/json/serialize/jPub.ts
+++ Q/src/json/serialize/jPub.ts
@@ -7,6 +7,13 @@ import { JPub, JPubPlayers } from '../data/data.types';
 import { toJMap } from './jMap';
 import { toJPlayer } from './jPlayer';
 
+/**
+ * Serialize a game state to a JPub object for a given player
+ *
+ * @param gameState the game state to serialize
+ * @param playerName the name of the player who's turn it is
+ * @returns a JPub object
+ */
 export function toJPub(
   gameState: RenderableGameState<ShapeColorTile>,
   playerName: string
@@ -21,6 +28,13 @@ export function toJPub(
   };
 }
 
+/**
+ * Serialize a list of players to a JPubPlayers object for a given player
+ *
+ * @param players the players to serialize
+ * @param playerName the name of the player who's turn it is
+ * @returns a JPubPlayers object
+ */
 function toJPubPlayers(
   players: RenderablePlayer<ShapeColorTile>[],
   playerName: string
