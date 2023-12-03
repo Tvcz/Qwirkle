065a70891a821da2d82ffa1719634df0010e6d02
Joe Hirsch <hirsch.jos@northeastern.edu>
2023-12-03 15:13:41 -0500

    fix broken toJMap

--- Q/src/json/deserialize/qMap.ts
+++ Q/src/json/deserialize/qMap.ts
@@ -8,16 +8,19 @@ export function toQMap(jMap: JMap): QMap<ShapeColorTile> {
   const tilePlacements: TilePlacement<ShapeColorTile>[] = [];
   jMap.forEach((row) => {
     (row.slice(1) as JCell[]).forEach((cell) =>
-      tilePlacements.push(toTilePlacement(cell))
+      tilePlacements.push(toTilePlacement(cell, row[0]))
     );
   });
   return new BaseMap(tilePlacements);
 }
 
-function toTilePlacement(jCell: JCell): TilePlacement<ShapeColorTile> {
+function toTilePlacement(
+  jCell: JCell,
+  rowIndex: number
+): TilePlacement<ShapeColorTile> {
   return {
     tile: toQTile(jCell[1]),
-    coordinate: new Coordinate(jCell[0], jCell[0])
+    coordinate: new Coordinate(jCell[0], rowIndex)
   };
 }
 
