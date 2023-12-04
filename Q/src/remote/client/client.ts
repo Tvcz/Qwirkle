a72391b5da9edbb00eeb2671c710643fabb754e5
Joe Hirsch <hirsch.jos@northeastern.edu>
2023-12-03 23:49:45 -0500

    allow client to start first

--- Q/src/remote/client/client.ts
+++ Q/src/remote/client/client.ts
@@ -9,20 +9,23 @@ import { toQPlayers } from '../../json/deserialize/qActor';
 import { BaseRuleBook } from '../../game/rules/ruleBook';
 import { toMs } from '../../utils';
 
-export function runClient(config: ClientConfig) {
+export async function runClient(config: ClientConfig) {
   const players = toQPlayers(config.players, new BaseRuleBook());
   const connectionOptions = {
     host: config.host,
     port: config.port
   };
-  players.forEach((player, index) => {
+
+  await joinGame(players[0], connectionOptions, !config.quiet);
+
+  players.splice(1).forEach((player, index) => {
     setTimeout(
       () => {
         !config.quiet &&
           console.error(`joining game as player ${config.players[index][0]}`);
         joinGame(player, connectionOptions, !config.quiet);
       },
-      toMs(config.wait * index)
+      toMs(config.wait * (index + 1))
     );
   });
 }
@@ -32,18 +35,50 @@ export function runClient(config: ClientConfig) {
  * Called once per player connecting to game.
  * @param player The player to join the game.
  * @param connectionOptions The host and port to connect to.
+ * @param shouldLog Whether to log the game state to the console.
  */
 export function joinGame(
   player: Player<ShapeColorTile>,
   connectionOptions = DEFAULT_CONNECTION_OPTIONS,
   shouldLog: boolean
-) {
-  // send initial message to server to join game
-  const socket = createConnection(connectionOptions);
-  socket.on('connect', () => {
-    // use server response to build connection
-    const connection = new TCPConnection(socket);
-    // use connection to create refereeProxy and hand off player instance
-    refereeProxy(player, connection, shouldLog);
+): Promise<void> {
+  return new Promise((resolve) => {
+    // send initial message to server to join game
+    const socket = createConnection(connectionOptions);
+    socket.on('connect', () => {
+      // use server response to build connection
+      const connection = new TCPConnection(socket);
+      // use connection to create refereeProxy and hand off player instance
+      refereeProxy(player, connection, shouldLog);
+      resolve();
+    });
+    socket.on('error', () => {
+      setTimeout(
+        () => resolve(joinGame(player, connectionOptions, shouldLog)),
+        1000
+      );
+    });
   });
 }
+
+// /**
+//  * Continues to attempt to join a game hosted at the specified host and port.
+//  * @param player The player to join the game.
+//  * @param connectionOptions The host and port to connect to.
+//  * @param shouldLog Whether to log the game state to the console.
+//  */
+// export function retryJoiningGame(
+//   player: Player<ShapeColorTile>,
+//   connectionOptions = DEFAULT_CONNECTION_OPTIONS,
+//   shouldLog: boolean
+// ) {
+//   try {
+//     joinGame(player, connectionOptions, shouldLog);
+//   } catch (e) {
+//     console.error(e);
+//     setTimeout(
+//       () => retryJoiningGame(player, connectionOptions, shouldLog),
+//       1000
+//     );
+//   }
+// }
