20177610d9e01aa05495edad4c3f051337baf890
Nicolas Araujo <araujo.n@northeastern.edu>
2023-12-08 16:32:47 -0500

    add JSDoc for message type guards

--- Q/src/json/messages/messagesTypeGuards.ts
+++ Q/src/json/messages/messagesTypeGuards.ts
@@ -13,6 +13,12 @@ import {
   WinResponse
 } from './messages.types';
 
+/**
+ * Checks if the given object is a method call.
+ *
+ * @param obj the object to check
+ * @returns true if the object is a method call, false otherwise
+ */
 function isMethodCall(obj: unknown): obj is MethodCall {
   return (
     typeof obj === 'object' &&
@@ -23,14 +29,29 @@ function isMethodCall(obj: unknown): obj is MethodCall {
   );
 }
 
+/**
+ * Checks if the given object is a name call.
+ * @param obj the object to check
+ * @returns true if the object is a name call, false otherwise
+ */
 export function isNameCall(obj: unknown): obj is NameCall {
   return isMethodCall(obj) && obj[0] === 'name' && obj[1].length === 0;
 }
 
+/**
+ * Checks if the given object is a name response.
+ * @param obj the object to check
+ * @returns true if the object is a name response, false otherwise
+ */
 export function isNameResponse(obj: unknown): obj is NameResponse {
   return typeof obj === 'string';
 }
 
+/**
+ * Checks if the given object is a set up call.
+ * @param obj the object to check
+ * @returns true if the object is a set up call, false otherwise
+ */
 export function isSetUpCall(obj: unknown): obj is SetUpCall {
   return (
     isMethodCall(obj) &&
@@ -42,10 +63,20 @@ export function isSetUpCall(obj: unknown): obj is SetUpCall {
   );
 }
 
+/**
+ * Checks if the given object is a set up response.
+ * @param obj the object to check
+ * @returns true if the object is a set up response, false otherwise
+ */
 export function isSetUpResponse(obj: unknown): obj is SetUpResponse {
   return typeof obj === 'string' && obj === 'void';
 }
 
+/**
+ * Checks if the given object is a take turn call.
+ * @param obj the object to check
+ * @returns true if the object is a take turn call, false otherwise
+ */
 export function isTakeTurnCall(obj: unknown): obj is TakeTurnCall {
   return (
     isMethodCall(obj) &&
@@ -55,10 +86,20 @@ export function isTakeTurnCall(obj: unknown): obj is TakeTurnCall {
   );
 }
 
+/**
+ * Checks if the given object is a take turn response.
+ * @param obj the object to check
+ * @returns true if the object is a take turn response, false otherwise
+ */
 export function isTakeTurnResponse(obj: unknown): obj is TakeTurnResponse {
   return isJChoice(obj);
 }
 
+/**
+ * Checks if the given object is a new tiles call.
+ * @param obj the object to check
+ * @returns true if the object is a new tiles call, false otherwise
+ */
 export function isNewTilesCall(obj: unknown): obj is NewTilesCall {
   return (
     isMethodCall(obj) &&
@@ -69,10 +110,20 @@ export function isNewTilesCall(obj: unknown): obj is NewTilesCall {
   );
 }
 
+/**
+ * Checks if the given object is a new tiles response.
+ * @param obj the object to check
+ * @returns true if the object is a new tiles response, false otherwise
+ */
 export function isNewTilesResponse(obj: unknown): obj is NewTilesResponse {
   return typeof obj === 'string' && obj === 'void';
 }
 
+/**
+ * Checks if the given object is a win call.
+ * @param obj the object to check
+ * @returns true if the object is a win call, false otherwise
+ */
 export function isWinCall(obj: unknown): obj is WinCall {
   return (
     isMethodCall(obj) &&
@@ -82,6 +133,11 @@ export function isWinCall(obj: unknown): obj is WinCall {
   );
 }
 
+/**
+ * Checks if the given object is a win response.
+ * @param obj the object to check
+ * @returns true if the object is a win response, false otherwise
+ */
 export function isWinResponse(obj: unknown): obj is WinResponse {
   return typeof obj === 'string' && obj === 'void';
 }
